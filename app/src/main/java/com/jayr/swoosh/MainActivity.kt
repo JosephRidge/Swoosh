18c8c7839456cd57b6a28fd24a273a610c6d0322
josephridge <josephridge18@gmail.com>
2022-03-29 16:45:32 +0300

    Implementing and explicit intent to navigate to another activity

--- app/src/main/java/com/jayr/swoosh/MainActivity.kt
+++ app/src/main/java/com/jayr/swoosh/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.jayr.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ToggleButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val getStartedButton = findViewById<ToggleButton>(R.id.getstarted_button)
+        getStartedButton.setOnClickListener {
+            val leagueContent = Intent(this, LeagueActivity::class.java)
+            startActivity(leagueContent)
+        }
     }
 }
